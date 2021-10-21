3c15baf54ab93b9d6f36aa881463a2d86f343cce
goodcorretora <goodcorretora@gmail.com>
2021-10-20 23:54:44 -0300

    create angular
    Install @angular/cli, @angular/material and create components.

---
+++ site/src/app/courses/courses/courses.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+
+  courses: any[] = [];
+
+  constructor() {
+    //this.courses = [];
+  }
+
+  ngOnInit(): void {
+  }
+
+}
